2d1c7ef2a559f3060c2efd4053b9b78f6620a57c
Paul_C <psc000001@gmail.com>
2023-04-29 16:04:29 +0100

    added an api call to stockbox with test api

--- client/src/containers/StockBox.js
+++ client/src/containers/StockBox.js
@@ -1,29 +1,55 @@
-import Nav from "../components/Nav";
-import Chart from "../components/Chart";
-import Search from "../components/Search";
-import BuyPanel from "../components/BuyPanel";
-import NewsPanel from "../components/NewsPanel";
-import StockDetailsItem from "../components/StockDetailsItem";
+import React, { useEffect, useState, useCallback } from 'react';
 
-const StockBox = ({selectedStock}) => {
-    if (!selectedStock){
-        return <p>No Stock Selected</p>
+import Nav from '../components/Nav';
+import Chart from '../components/Chart';
+import Search from '../components/Search';
+import BuyPanel from '../components/BuyPanel';
+import NewsPanel from '../components/NewsPanel';
+import StockDetailsItem from '../components/StockDetailsItem';
+
+const StockBox = ({ selectedStock, globalSelectedStockData, fetchGlobalSelectedStockData, setGlobalSelectedStockData }) => {
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (selectedStock) {
+      fetchGlobalSelectedStockData(selectedStock.symbol);
     }
-    return ( 
-        <>
-            <p>StockBox</p> 
-            {selectedStock.name}
-            <Nav></Nav>
-            <Search></Search>
-            <Chart></Chart>
-            <BuyPanel></BuyPanel>
-            <NewsPanel></NewsPanel>
-            <StockDetailsItem></StockDetailsItem>
-            <hr></hr>
+  }, [selectedStock, fetchGlobalSelectedStockData]);
 
-        </>
+  useEffect(() => {
+    if (!selectedStock) {
+      setGlobalSelectedStockData(null);
+      setError(null);
+    }
+  }, [selectedStock]);
 
-     );
-}
- 
+  if (!selectedStock) {
+    return <p>No Stock Selected</p>;
+  }
+
+  return (
+    <>
+      <h3>StockBox</h3>
+      {selectedStock.name}
+      <Nav />
+      <Search />
+      <Chart />
+      <BuyPanel />
+      <NewsPanel />
+      {error ? (
+        <p>Error: {error.message}</p>
+        ) : (
+        <p>
+            Price:{' '}
+            {globalSelectedStockData && globalSelectedStockData['Global Quote'] && globalSelectedStockData['Global Quote']['05. price']}
+            Change:{' '}
+            {globalSelectedStockData && globalSelectedStockData['Global Quote'] && globalSelectedStockData['Global Quote']['10. change percent']}
+
+        </p>
+      )}
+      <hr />
+    </>
+  );
+};
+
 export default StockBox;
