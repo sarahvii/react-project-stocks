2f0cf9ff1f4cc0420789fef4ffee8d75ec9f7974
Sarah Smith <sarahsmithvii@gmail.com>
2023-04-29 22:50:24 +0100

    form and buy button added

--- client/src/components/BuyPanel.js
+++ client/src/components/BuyPanel.js
@@ -1,8 +1,50 @@
+import React, { useEffect } from 'react';
+
 const BuyPanel = () => {
-    return ( 
-        <p>BuyPanel</p>
-     );
-}
- 
+
+    const handleButtonClick = () => {
+        console.log('Button has been clicked');
+    };
+
+    const handleFormSubmit = function (event) {
+        event.preventDefault();
+        console.log(event.target.holdings.value);
+    };
+
+    useEffect(() => {
+        const form = document.querySelector('#buy_form');
+        const button = document.querySelector('#buy_button');
+
+        form.addEventListener('submit', handleFormSubmit);
+        button.addEventListener('click', handleButtonClick);
+
+        return () => {
+            form.removeEventListener('submit', handleFormSubmit);
+            button.removeEventListener('click', handleButtonClick);
+        };
+    }, []);
+
+    return (
+        <>
+        <p>Buy Panel</p>
+        <form id="buy_form">
+            <label>Amount to buy:
+                <input type="text" name="holdings" />
+            </label>
+        <button id="buy_button">Buy</button>
+        </form>
+        </>
+    );
+
+
+
+};
+
+
+
+
+
+
+
 export default BuyPanel
 ;
