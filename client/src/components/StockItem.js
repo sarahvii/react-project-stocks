191675fb2b884f5a8a27ff71f16e053a2989c48c
Paul_C <psc000001@gmail.com>
2023-05-01 21:33:24 +0100

    added some details to the StockItem as well calculation functions for detailing

--- client/src/components/StockItem.js
+++ client/src/components/StockItem.js
@@ -1,20 +1,46 @@
 import { Link } from 'react-router-dom'
 
-const StockItem = ({portfolioStock, selectedStock, setSelectedStock}) => {
+const StockItem = ({portfolioStock, selectedStock, setSelectedStock, currentPrice}) => {
     console.log("Portfolio stock in StockItem:", portfolioStock);
 
     const handleClick = (portfolioStock) => {
         console.log("click")
         setSelectedStock(portfolioStock)
         console.log("selectedStock is now:", selectedStock)
-
-
     }
 
+    const calculatePriceDiff = (portfolioStock) => {
+        console.log("currentPrice:", currentPrice);
+        console.log("price_purchased:", portfolioStock.price_purchased);
+        let profit_or_loss = currentPrice - portfolioStock.price_purchased;
+        return profit_or_loss;
+    }
+
+    const calculatePercentageDiff = (portfolioStock) => {
+        let percentage_diff = ((currentPrice - portfolioStock.price_purchased) / portfolioStock.price_purchased) * 100;
+        return percentage_diff
+    }
+
+    const calculateHoldingsValue = (portfolioStock) => {
+        let holdingsValue = (portfolioStock.holdings * currentPrice)
+        return holdingsValue
+    }
+
+    const profit_or_loss = calculatePriceDiff(portfolioStock)
+    const percentage_diff = calculatePercentageDiff(portfolioStock)
+    const holdingsValue = calculateHoldingsValue(portfolioStock)
+
+
+
+
+
     return ( 
-        <Link to="/stocks">
-            <p onClick={()=>handleClick(portfolioStock)}>{portfolioStock.name}</p>
-        </Link>
+        <>
+         <Link to="/stocks"><span onClick={()=>handleClick(portfolioStock)}>{portfolioStock.name}</span></Link><span> <span>_/_ ${holdingsValue.toFixed(2)}</span></span>
+        <p>{portfolioStock.symbol} {portfolioStock.holdings} Shares  ${profit_or_loss.toFixed(2)} ({percentage_diff.toFixed(2)}%)</p>
+        </>
+
+
      );
 }
  
