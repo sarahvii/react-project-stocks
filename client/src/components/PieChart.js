4f2fdf15f582f8a591a509554d7c4ec1646627f6
ateight88 <doyleto@icloud.com>
2023-05-02 20:34:19 +0100

    added charts

--- client/src/components/PieChart.js
+++ client/src/components/PieChart.js
@@ -1,7 +1,39 @@
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import _ from 'lodash';
+
+const getOptions = type => ({
+  chart: {
+    type,
+    width: null,
+    height: (9 / 16) * 50 + '%',
+  },
+  title: {
+    text: _.startCase(`${type} chart`),
+  },
+  yAxis: {
+    title: {
+      text: 'Values',
+    },
+  },
+  series: [
+    {
+      data: [1, 2, 1, 4, 3, 6],
+    },
+    {
+      data: [2, 7, 0, 4, 6, 2],
+    },
+  ],
+});
+
 const PieChart = () => {
-    return ( 
-        <p>This is where the pie chart goes.</p>
-     );
-}
- 
+  return (
+    <div style={{ maxWidth: '80%', margin: '4rem' }}>
+      <h1>HighChartsStock.js</h1>
+
+      <HighchartsReact highcharts={Highcharts} options={getOptions('pie')} />
+    </div>
+  );
+};
+
 export default PieChart;
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,38 +1,67 @@
-import { useState } from 'react';
+// db data
 
-const symbolNames = [
-  ['AAPL', 'Apple Inc.'],
-  ['GOOGL', 'Alphabet Inc.'],
-  ['MSFT', 'Microsoft Corporation'],
-  ['META', 'Meta'],
-  ['AMZN', 'Amazon'],
-  ['NFLX', 'Netflix'],
-];
+import { useState, useEffect } from 'react';
 
-const Search = () => {
+const symbolNames = [];
+
+const FilteredSearch = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [showModal, setShowModal] = useState(false);
+  const [database, setDatabase] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:9000/api/stocks/');
+        const data = await response.json();
+        setDatabase(data);
+        console.log('Response:', data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // useEffect(() => {
+  //   // Fetch the data from the database
+  //   fetch('http://localhost:9000/api/stocks/')
+  //     .then(response => response.json())
+  //     .then(data => setDatabase(data))
+  //     .catch(error => console.error(error));
+  // }, [database]);
 
   const handleSearchChange = event => {
     setSearchTerm(event.target.value);
   };
 
-  // const handleSubmit = event => {
-  //   event.preventDefault();
-  //   console.log(`Submit with search term: ${searchTerm}`);
-  // };
-
   const handleSubmit = event => {
     event.preventDefault();
-    const filteredSymbols = symbolNames.filter(([symbol, name]) =>
+    const filteredSymbols = database.filter(({ name }) =>
       name.toLowerCase().includes(searchTerm.toLowerCase())
     );
-    console.log('Filtered symbols:', filteredSymbols);
+    const symbols = filteredSymbols.map(({ symbol, name }) => [symbol, name]);
+    console.log('Filtered symbols:', symbols);
+    // Add the filtered symbols to the symbolNames array
+    const newSymbolNames = symbolNames.concat(symbols);
+    setSearchTerm('');
+    console.log('New symbolNames:', newSymbolNames);
   };
 
-    const filteredSymbols = symbolNames.filter(([symbol, name]) =>
-    name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const filteredSymbols = Object.entries(database)
+    .filter(([key, { name }]) =>
+      name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    .map(([key, { symbol, name }]) => [symbol, name]);
+
+  // const filteredSymbols = database.filter(({ name }) =>
+  //   name.toLowerCase().includes(searchTerm.toLowerCase())
+  // );
+
+  // const filteredSymbols = symbolNames.filter(([symbol, name]) =>
+  //   name.toLowerCase().includes(searchTerm.toLowerCase())
+  // );
 
   const handleShowModal = () => {
     setShowModal(true);
@@ -44,9 +73,7 @@ const Search = () => {
 
   return (
     <div>
-
-      <h2>This is the filtered search bar.</h2>
-
+      <h1>FilteredSearchDB.js</h1>
       <p>Filter by company name:</p>
 
       {!showModal ? (
@@ -87,10 +114,103 @@ const Search = () => {
   );
 };
 
-export default Search;
+export default FilteredSearch;
+
+// static data
+// import { useState } from 'react';
+
+// const symbolNames = [
+//   ['AAPL', 'Apple Inc.'],
+//   ['GOOGL', 'Alphabet Inc.'],
+//   ['MSFT', 'Microsoft Corporation'],
+//   ['META', 'Meta'],
+//   ['AMZN', 'Amazon'],
+//   ['NFLX', 'Netflix'],
+// ];
+
+// const Search = () => {
+//   const [searchTerm, setSearchTerm] = useState('');
+//   const [showModal, setShowModal] = useState(false);
+
+//   const handleSearchChange = event => {
+//     setSearchTerm(event.target.value);
+//   };
+
+//   // const handleSubmit = event => {
+//   //   event.preventDefault();
+//   //   console.log(`Submit with search term: ${searchTerm}`);
+//   // };
+
+//   const handleSubmit = event => {
+//     event.preventDefault();
+//     const filteredSymbols = symbolNames.filter(([symbol, name]) =>
+//       name.toLowerCase().includes(searchTerm.toLowerCase())
+//     );
+//     console.log('Filtered symbols:', filteredSymbols);
+//   };
+
+//     const filteredSymbols = symbolNames.filter(([symbol, name]) =>
+//     name.toLowerCase().includes(searchTerm.toLowerCase())
+//   );
+
+//   const handleShowModal = () => {
+//     setShowModal(true);
+//   };
+
+//   const handleCloseModal = () => {
+//     setShowModal(false);
+//   };
+
+//   return (
+//     <div>
+
+//       <h2>This is the filtered search bar.</h2>
+
+//       <p>Filter by company name:</p>
+
+//       {!showModal ? (
+//         <button type='button' onClick={handleShowModal}>
+//           Show List
+//         </button>
+//       ) : (
+//         <button type='button' onClick={handleCloseModal}>
+//           Close
+//         </button>
+//       )}
+//       {showModal && (
+//         <div className='modal'>
+//           <div className='modal-content'>
+//             <form onSubmit={handleSubmit}>
+//               <label>
+//                 Filter by company name:
+//                 <input
+//                   type='text'
+//                   value={searchTerm}
+//                   onChange={handleSearchChange}
+//                 />
+//               </label>
+//               <button type='submit'>Search</button>
+//             </form>
+//             <ul>
+//               {filteredSymbols.map(([symbol, name]) => (
+//                 <li key={symbol}>{`${name} (${symbol})`}</li>
+//               ))}
+//             </ul>
+//             <button type='button' onClick={handleCloseModal}>
+//               Close
+//             </button>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Search;
 
 // import { useState } from 'react';
 
+// basic
 // const symbolNames = [
 //   ['AAPL', 'Apple Inc.'],
 //   ['GOOGL', 'Alphabet Inc.'],
